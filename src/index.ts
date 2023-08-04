c9bb79a7b8080203c3f5664f4b8d500cc16d96e9
initialencounter <2911583893@qq.com>
2023-08-05 00:22:00 +0800

    change: version

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import { Context, Schema, Logger } from 'koishi'
 import { createWriteStream } from 'fs'
-import { copyFileSync, existsSync, mkdirSync, promises as fs } from 'fs'
-import { mkdir, rm, writeFile } from 'fs/promises'
-import { join } from 'path'
+import { existsSync, promises as fs } from 'fs'
+import { mkdir } from 'fs/promises'
 import AdmZip from 'adm-zip'
 import axios from 'axios'
 import env from 'env-paths'
-import { dirname, resolve } from 'path'
+import { resolve } from 'path'
 import { spawn, SpawnOptions } from 'child_process'
-import { version } from 'os'
-import { log } from 'console'
+import { extract } from 'tar'
 
 export const name = 'qsign'
 export const logger = new Logger(name)
@@ -18,22 +16,19 @@ export interface Config {
 }
 
 export const Config: Schema<Config> = Schema.object({
-  version: Schema.string().description('版本选择').description('v31'),
+  version: Schema.string().description('版本选择').default('v1.1.1-dev'),
 })
 
-export async function apply(ctx: Context, config: Config) {
-  const basename = `go-cqhttp_${getPlatform()}_${getArch()}${process.platform === 'win32' ? '.exe' : ''}`
+export async function apply(ctx:Context, config: Config) {
+  const basename = `go-cqhttp${process.platform === 'win32' ? '.exe' : ''}`
   const binary = resolve(env('gocqhttp').data, config.version, basename)
-  const cwd = join(__dirname, '../temp')
-  
-  if(!existsSync(binary)){
+  if (!existsSync(binary)) {
     const platform = getPlatform(process.platform)
     const arch = getArch(process.arch)
-    await downloadRelease(cwd, platform, arch, config.version)
-    rm(cwd,{recursive:true})
+    await downloadRelease(platform, arch, config.version)
   }
   logger.info('环境准备就绪！')
-  
+
 }
 
 export function getArch(arch: string = process.arch) {
@@ -55,47 +50,51 @@ export function getPlatform(platform: string = process.platform) {
   throw new Error(`Unsupported platform: ${platform}`)
 }
 
-export function getEnvPath(version:string){
+export function getEnvPath(version: string) {
   const basename = `go-cqhttp_${getPlatform()}_${getArch()}${process.platform === 'win32' ? '.exe' : ''}`
   return resolve(env('gocqhttp').data, version, basename)
 }
 
-export async function downloadRelease(cwd: string, platform: string, arch: string, version: string) {
-  // https://gitee.com/initencunter/go-cqhttp-dev/releases/download/v31/darwin_arm64.zip
-  const filename = `${platform}_${arch}.zip`
+export async function downloadRelease(platform: string, arch: string, version: string) {
+  // https://gitee.com/initencunter/go-cqhttp-dev/releases/download/v1.1.1-dev/go-cqhttp-windows-amd64.zip
+  const filename = `go-cqhttp-${platform}-${arch}.${(getPlatform() === "windows" ? "zip" : "tar.gz")}`
   const url = `https://gitee.com/initencunter/go-cqhttp-dev/releases/download/${version}/${filename}`
 
   logger.info(`正在安装 go-cqhttp ${url}`)
   const [{ data: stream }] = await Promise.all([
     axios.get<NodeJS.ReadableStream>(url, { responseType: 'stream' }),
-    mkdir(cwd, { recursive: true }),
   ])
-  return new Promise<void>((resolved, reject) => {
-    stream.on('end', resolve)
+  return new Promise<void>(async (resolved, reject) => {
+    stream.on('end', resolved)
     stream.on('error', reject)
-    stream.pipe(createWriteStream(resolve(cwd, `${version}.zip`))).on('finish', () => {
-      const gocqpath = resolve(env('gocqhttp').data)
-      const adm = new AdmZip(resolve(cwd, `${version}.zip`));
-      adm.extractAllTo(resolve(gocqpath, version), true);
-      //const backup = resolve(__dirname, '../bin', 'go-cqhttp' + (process.platform === 'win32' ? '.exe' : ''))
-      //copyFileSync(gocqpath,backup)
-      resolved();
-    })
-      .on('error', (error) => {
-        reject(error);
-      });
+    const gocqpath = resolve(env('gocqhttp').data)
+    logger.info(gocqpath)
+    // windows
+    if (filename.endsWith('.zip')) {
+      const gocqpath2 = resolve(gocqpath,version)
+      await mkdir(gocqpath2, { recursive: true }),
+      stream.pipe(createWriteStream(resolve(gocqpath2,'go-cqhttp.zip'))).on("finish",()=>{
+        const adm = new AdmZip(resolve(gocqpath2,'go-cqhttp.zip'))
+        adm.extractAllTo(gocqpath2,true)
+      }).on("error",(err)=>{
+        reject(err)
+      })    
+    } else {
+      stream.pipe(extract({ gocqpath , newer: true }, ['go-cqhttp']))
+      const cmd = `chmod x+ ${resolve(gocqpath,'go-cqhttp')}`
+      logger.info(cmd)
+      spawn(cmd)
+    }
+
   })
 }
 
 
-export function gocq(options: gocq.Options = {},version:string) {
-  const basename = `go-cqhttp_${getPlatform()}_${getArch()}${process.platform === 'win32' ? '.exe' : ''}`
+export function gocq(options: gocq.Options = {}, version: string) {
+  const basename = `go-cqhttp${process.platform === 'win32' ? '.exe' : ''}`
   const binary = resolve(env('gocqhttp').data, version, basename)
   const backup = resolve(__dirname, '../bin', basename)
   const args: string[] = []
-  if (!existsSync(resolve(join(__dirname, '../temp'),`${version}.zip`))) {
-    logger.info('警告！备份文件丢失！')
-  }
   logger.info(binary)
   if (options.faststart) args.push('-faststart')
   return spawn(binary, args, {
@@ -112,14 +111,4 @@ namespace gocq {
   export interface Options extends SpawnOptions {
     faststart?: boolean
   }
-}
-
-function getFileSize(filePath:string) {
-  return new Promise((resolve, reject) => {
-    fs.stat(filePath).then((stat)=>{
-      resolve(stat.size)
-    }).catch((e)=>{
-      reject(e)
-    })
-  });
 }
---
+++ src/install.ts
@@ -0,0 +1,3 @@
+import {apply} from './index'
+import {Context} from 'koishi'
+apply(new Context(),{version:"v1.1.1-dev"})
