1c3ad1248970f09e229aa1bbed7682b4c13826b3
initialencounter <2911583893@qq.com>
2023-08-05 14:43:16 +0800

    change: readme

--- readme.md
+++ readme.md
@@ -3,6 +3,14 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-qsign?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-qsign)
 
 
-启用即可自动安装 go-cqhttp-dev
+启用即可自动安装 [go-cqhttp-dev](https://github.com/rhwong/go-cqhttp-dev)
+
+专属启动器 @initencounter/koishi-plugin-gocqhttp
+
+由于 go-cqhttp-dev 的 [releases](https://github.com/rhwong/go-cqhttp-dev/releases/tag/v1.1.1-dev) 中不存在 darwin, 目前只支持 windows 和 linux 平台
 
 本插件抄袭自[node-gocqhttp](https://github.com/koishijs/node-gocqhttp)
+
+## 问题反馈
+* QQ群：399899914<br>
+* 小伙伴如果遇到问题或者有新的想法，欢迎到[这里](https://github.com/initialencounter/koishi-plugin-qsign/issues)反馈哦~
--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,28 @@ export const logger = new Logger(name)
 export interface Config {
   version: string
 }
-
 export const Config: Schema<Config> = Schema.object({
   version: Schema.string().description('版本选择').default('v1.1.1-dev'),
 })
+
+export const usage = `
+# koishi-plugin-qsign
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-qsign?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-qsign)
+
+
+启用即可自动安装 [go-cqhttp-dev](https://github.com/rhwong/go-cqhttp-dev)
+
+专属启动器 @initencounter/koishi-plugin-gocqhttp
+
+由于 go-cqhttp-dev 的 [releases](https://github.com/rhwong/go-cqhttp-dev/releases/tag/v1.1.1-dev) 中不存在 darwin, 目前只支持 windows 和 linux 平台
+
+本插件抄袭自[node-gocqhttp](https://github.com/koishijs/node-gocqhttp)
+
+## 问题反馈
+* QQ群：399899914<br>
+* 小伙伴如果遇到问题或者有新的想法，欢迎到[这里](https://github.com/initialencounter/koishi-plugin-qsign/issues)反馈哦~
+`
 
 export async function apply(ctx: Context, config: Config) {
   const basename = `go-cqhttp${process.platform === 'win32' ? '.exe' : ''}`
@@ -104,7 +122,6 @@ export async function downloadRelease(platform: string, arch: string, version: s
 export function gocq(options: gocq.Options = {}, version: string) {
   const basename = `go-cqhttp${process.platform === 'win32' ? '.exe' : ''}`
   const binary = resolve(env('gocqhttp').data, version, basename)
-  const backup = resolve(__dirname, '../bin', basename)
   const args: string[] = []
   logger.info(binary)
   if (options.faststart) args.push('-faststart')
